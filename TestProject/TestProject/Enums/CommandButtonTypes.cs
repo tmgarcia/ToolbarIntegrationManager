f4cfe0ba7e09bd35aeeb548a84a266a26b31c29a
Taylor Garcia <tclustuff@gmail.com>
2014-09-30 13:51:32 -0600

    Adding command bars and some updated icon images

---
+++ TestProject/TestProject/Enums/CommandButtonTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Enums
+{
+    public enum CommandButtonTypes
+    {
+        ToolbarIconClose, ToolbarIconMinimize, ToolbarIconMaximize, ToolbarIconHelp,
+    }
+}
---
+++ TestProject/TestProject/Models/CommandButton.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using TestProject.Enums;
+using TestProject.UserControls;
+
+namespace TestProject.Models
+{
+    class CommandButton
+    {
+        CommandButtonTypes type;
+        string normalPath;
+        string hoverPath;
+        string pressedPath;
+        ThreeStateEnabledButton button;
+
+        public CommandButton(CommandButtonTypes buttonType)
+        {
+            type = buttonType;
+            SetupTypePaths();
+        }
+
+        public Button GetButtonControl()
+        {
+            return button.buttonControl;
+        }
+
+        public void Disable()
+        {
+            button.disable();
+        }
+        public void Enable()
+        {
+            button.enable();
+        }
+
+        private void SetupTypePaths()
+        {
+            switch (type)
+            {
+                case CommandButtonTypes.ToolbarIconClose:
+                    normalPath = "Images/commandButtonToolbarIconCloseN.png";
+                    hoverPath = "Images/commandButtonToolbarIconCloseH.png";
+                    pressedPath = "Images/commandButtonToolbarIconCloseP.png";
+                    break;
+                case CommandButtonTypes.ToolbarIconMinimize:
+                    normalPath = "Images/commandButtonToolbarIconMinimizeN.png";
+                    hoverPath = "Images/commandButtonToolbarIconMinimizeH.png";
+                    pressedPath = "Images/commandButtonToolbarIconMinimizeP.png";
+                    break;
+                case CommandButtonTypes.ToolbarIconMaximize:
+
+                    break;
+                case CommandButtonTypes.ToolbarIconHelp:
+
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    }
+}
